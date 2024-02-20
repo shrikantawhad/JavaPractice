6f7cb9770fdfeb1ee25b8df8f4df37ae3b95ea11
Shrikant <shrikant.awhad.58@gmail.com>
2024-02-20 15:36:24 +0530

    Added Collection Package

---
+++ src/Collection/list/ListDemo.java
@@ -0,0 +1,40 @@
+package Collection.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo {
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<Integer>();
+
+		list.add(12);
+		System.out.println(list);
+
+		list.clear();
+		list.add(12);
+		list.add(13);
+		list.add(17);
+		list.add(15);
+		list.add(16);
+		list.add(17);
+
+		System.out.println(list);
+		System.out.println(list.lastIndexOf(17));
+		System.out.println(list.indexOf(17));
+		System.out.println(list.get(3));
+		list.sort(null);
+	
+		
+		list.stream().map(n->n*2).forEach(j->System.out.println(j));
+
+		list.set(5, 87);
+		list.remove(0);
+		list.remove((Object) 15);
+		System.out.println(list);
+		
+		list.toArray(new String[list.size()]);
+	
+	
+	}
+
+}
